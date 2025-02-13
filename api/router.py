c3535578aaa2949f768aec1ff31235fe2a054086
SPaci <68151600+SPaci20@users.noreply.github.com>
2025-02-14 00:02:59 +0200

    chore: Minor update on FastAPI

--- api/router.py
+++ api/router.py
@@ -1,6 +1,16 @@
 from fastapi import APIRouter
-
+from fastapi.responses import JSONResponse
 from api.routes import books
 
 api_router = APIRouter()
+
+# Root route
+@api_router.get("/")
+async def root():
+    return JSONResponse(
+        content={"message": "Welcome to the FastAPI Book Management API!"},
+        status_code=200,
+    )
+
+# Include the books router
 api_router.include_router(books.router, prefix="/books", tags=["books"])
